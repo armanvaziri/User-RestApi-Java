4a0ed7f42ac6eeab97cf36306b56290ccf2278e1
Arman <arman.vaziri1@gmail.com>
2019-07-16 17:14:58 -0700

    First commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user(name, age, eth, occ) VALUES('Arman Vaziri', 20, 'Iranian', 'Student');
+INSERT INTO user(name, age, eth, occ) VALUES('Barrack Obama', 57, 'African-American', 'FPOTUS');
+
+COMMIT;
